a50b80505ddc23e35db19d6fcf30ae7030ae5ff7
Stuart Preston <stuart@pendrica.com>
2015-11-18 12:31:22 +0000

    Tidy up tslint warnings

--- extension.ts
+++ extension.ts
@@ -1,16 +1,14 @@
-import path = require('path');
-import vscode = require('vscode');
+import path = require("path");
+import vscode = require("vscode");
 
 let diagnosticCollection: vscode.DiagnosticCollection;
 let config: vscode.WorkspaceConfiguration;
 let rubocopPath: string;
 
 export function activate(context: vscode.ExtensionContext): void {
-	
 	diagnosticCollection = vscode.languages.createDiagnosticCollection("ruby");
 	context.subscriptions.push(diagnosticCollection);
-	
-	if(vscode.workspace.getConfiguration('rubocop')['path'] == "") {
+	if(vscode.workspace.getConfiguration("rubocop")["path"] == "") {
 		if (process.platform == "win32") {
 			rubocopPath = "C:\\opscode\\chefdk\\bin\\rubocop.bat";
 		} else {
@@ -18,14 +16,14 @@ export function activate(context: vscode.ExtensionContext): void {
 		}
 	}
 	else
-	{		
-		rubocopPath = vscode.workspace.getConfiguration('rubocop')['path'];
-		console.log("custom rubocop path:" + rubocopPath);
+	{
+		rubocopPath = vscode.workspace.getConfiguration("rubocop")["path"];
+		console.log("Using custom Rubocop path:" + rubocopPath);
 	}
-	
-	if(vscode.workspace.getConfiguration('rubocop')['enable']) {
+	if(vscode.workspace.getConfiguration("rubocop")["enable"]) {
 		validateWorkspace();
 		context.subscriptions.push(startLintingOnSaveWatcher());
+		context.subscriptions.push(startLintingOnConfigurationChangeWatcher());
 	}
 }
 
@@ -40,20 +38,12 @@ function convertSeverity(severity: string): vscode.DiagnosticSeverity {
 	}
 }
 
-function fileUrl(str): string {
-    let pathName = path.resolve(str).replace(/\\/g, "/");
-    if (pathName[0] !== "/") {
-        pathName = "/" + pathName;
-    }
-    return encodeURI("file://" + pathName);
-};
-
 function validateWorkspace(): void {
 	try {
 		let spawn = require("child_process").spawnSync;
 		let rubocop = spawn(rubocopPath, ["-f", "j", vscode.workspace.rootPath], { cwd: vscode.workspace.rootPath });
 		let rubocopOutput = JSON.parse(rubocop.stdout);
-		let arr = []; 
+		let arr = [];
 		for (var r = 0; r < rubocopOutput.files.length; r++) {
 			var rubocopFile = rubocopOutput.files[r];
 			let uri: vscode.Uri = vscode.Uri.file((path.join(vscode.workspace.rootPath, rubocopFile.path)));
@@ -64,7 +54,7 @@ function validateWorkspace(): void {
 				let _start = parseInt(offenses[i].location.column) - 1;
 				let _end = parseInt(_start + offenses[i].location.length);
 				let diagRange = new vscode.Range(_line, _start, _line, _end);
-				let diagMsg = `${offenses[i].message} (${offenses[i].cop_name})`; 
+				let diagMsg = `${offenses[i].message} (${offenses[i].cop_name})`;
 				let diagSeverity = convertSeverity(offenses[i].severity);
 				let diagnostic = new vscode.Diagnostic(diagRange, diagMsg, diagSeverity);
 				diagnostics.push(diagnostic);
@@ -82,13 +72,15 @@ function validateWorkspace(): void {
 
 function startLintingOnSaveWatcher() {
 	return vscode.workspace.onDidSaveTextDocument(document => {
-		
-		console.log(document.uri);
-		
 		if(document.languageId != "ruby") {
 			return;
 		}
-		
-		validateWorkspace();	
+		validateWorkspace();
+	});
+}
+
+function startLintingOnConfigurationChangeWatcher() {
+	return vscode.workspace.onDidChangeConfiguration(params => {
+		validateWorkspace();
 	});
 }
