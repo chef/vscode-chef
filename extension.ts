f1e3c43a16b270bebfe416adefbd0cb4a3821e01
Tim Smith <tsmith84@gmail.com>
2019-09-14 13:04:25 -0700

    Remove foodcritic support now that it's EOL
    Foodcritic has been replaced by Cookstyle now that Cookstyle handles
    both Ruby and Chef linting. This speeds up this extension as now we only
    need to run a single linting on code changes.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- extension.ts
+++ extension.ts
@@ -5,18 +5,14 @@ import path = require("path");
 import vscode = require("vscode");
 
 let diagnosticCollectionRubocop: vscode.DiagnosticCollection;
-let diagnosticCollectionFoodcritic: vscode.DiagnosticCollection;
 let config: vscode.WorkspaceConfiguration;
 let rubocopPath: string;
 let rubocopConfigFile: string;
-let foodcriticPath: string;
 let cookbookPaths: Array<string> = [];
 
 export function activate(context: vscode.ExtensionContext): void {
 	diagnosticCollectionRubocop = vscode.languages.createDiagnosticCollection("rubocop");
-	diagnosticCollectionFoodcritic = vscode.languages.createDiagnosticCollection("foodcritic");
 	context.subscriptions.push(diagnosticCollectionRubocop);
-	context.subscriptions.push(diagnosticCollectionFoodcritic);
 
 	if (vscode.workspace.getConfiguration("rubocop").path === "") {
 		if (process.platform === "win32") {
@@ -40,23 +36,6 @@ export function activate(context: vscode.ExtensionContext): void {
 		validateWorkspace();
 		context.subscriptions.push(startLintingOnSaveWatcher());
 		context.subscriptions.push(startLintingOnConfigurationChangeWatcher());
-	}
-
-	if (vscode.workspace.getConfiguration("foodcritic").path === "") {
-		if (process.platform === "win32") {
-			foodcriticPath = "C:\\opscode\\chefdk\\embedded\\bin\\foodcritic.bat";
-		} else {
-			foodcriticPath = "/opt/chefdk/embedded/bin/foodcritic";
-		}
-	} else {
-		foodcriticPath = vscode.workspace.getConfiguration("foodcritic").path;
-		console.log("Using custom Foodcritic path: " + foodcriticPath);
-	}
-
-	if (vscode.workspace.getConfiguration("foodcritic").enable) {
-		recalculateValidCookbookPaths();
-		context.subscriptions.push(startCookbookAnalysisOnSaveWatcher());
-		context.subscriptions.push(startCookbookAnalysisOnConfigurationChangeWatcher());
 	}
 }
 
@@ -146,62 +125,6 @@ function startLintingOnConfigurationChangeWatcher():any {
 	return vscode.workspace.onDidChangeConfiguration(params => {
 		console.log("Workspace configuration changed, validating workspace.");
 		validateWorkspace();
-	});
-}
-
-function validateCookbooks(): void {
-	console.log("in validate cookbooks");
-	if (cookbookPaths.length === 0) { return; }
-	try {
-		let spawn = require("child_process").spawnSync;
-		cookbookPaths[cookbookPaths.length] = "--no-progress";
-		let foodcritic = spawn(foodcriticPath, cookbookPaths, { cwd: vscode.workspace.rootPath, encoding: "utf8" });
-		if (foodcritic.status > 0) {
-			let foodcriticOutput = foodcritic.stdout.trim().split("\n");
-			let arr = [];
-			for (var r = 0; r < foodcriticOutput.length; r++) {
-				if (foodcriticOutput[r] !== "") {
-					let diagnostics: vscode.Diagnostic[] = [];
-					let fcId = foodcriticOutput[r].split(":")[0].trim();
-					let fcDescription = foodcriticOutput[r].split(":")[1].trim();
-					let fcPath = foodcriticOutput[r].split(":")[2].trim();
-					let lineNumber = parseInt(foodcriticOutput[r].split(":")[3].trim(), 10) - 1;
-					let uri: vscode.Uri = vscode.Uri.file((path.join(vscode.workspace.rootPath, fcPath)));
-					let diagRange = new vscode.Range(lineNumber, 0, lineNumber, 0);
-					let diagnostic = new vscode.Diagnostic(diagRange, fcId + ": " + fcDescription, vscode.DiagnosticSeverity.Warning);
-					let found: boolean = false;
-					diagnostics.push(diagnostic);
-					for (var l = 0; l < arr.length; l++) {
-						if (arr[l][0].path === uri.path) {
-							arr[l][1].push(diagnostic);
-							found = true;
-						}
-					}
-					if (!found) {
-						arr.push([uri, diagnostics]);
-					}
-				}
-			}
-
-			diagnosticCollectionFoodcritic.clear();
-			diagnosticCollectionFoodcritic.set(arr);
-		} else {
-			console.log("Foodcritic executed but exited with status: " + foodcritic.status + foodcritic.stdout);
-			diagnosticCollectionFoodcritic.clear();
-		}
-	} catch (err) {
-		console.log(err);
-	}
-	return;
-}
-
-function startCookbookAnalysisOnSaveWatcher():any {
-	return vscode.workspace.onDidSaveTextDocument(document => {
-		console.log("onDidSaveTextDocument event received (foodcritic).");
-		if (document.languageId !== "ruby") {
-			return;
-		}
-		validateCookbooks();
 	});
 }
 
